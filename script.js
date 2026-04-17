d27256f6427561639c97a627429e907c86b7d019
DevisedWeb <contact@devisedweb.com>
2026-04-17 22:55:49 +0500

    Update form validation

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const signupForm = document.querySelector("#newsletter-form");
+const formErrorMessage = document.querySelector(".form-error-message");
+const emailInput = document.querySelector("#email");
+
+emailInput.addEventListener("input", function () {
+  formErrorMessage.textContent = "";
+});
 
 function handleSignUpForm(e) {
   e.preventDefault();
@@ -9,8 +15,6 @@ function handleSignUpForm(e) {
   const emailErrorMessage = validateEmail(email);
 
   if (emailErrorMessage) {
-    const formErrorMessage = document.querySelector(".form-error-message");
-
     formErrorMessage.textContent = emailErrorMessage;
   }
 }
@@ -20,7 +24,7 @@ signupForm.addEventListener("submit", handleSignUpForm);
 function validateEmail(email) {
   if (!email) return "Email is required";
 
-  const isValidEmail = /^\S+@\S+$/g;
+  const isValidEmail = /^\S+@\S+$/;
   if (!isValidEmail.test(email)) {
     return "Valid email required";
   }
